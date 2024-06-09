8fc27423c4d3c955f69c15812b909dffc95dc0de
Jason <xjc04@qq.com>
2024-06-09 09:51:07 -0700

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231228.214325';
+const cacheName = 'chirpy-20240612.172018';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
