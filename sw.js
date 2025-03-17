dcd7320bbec3601db2011b7fb064da26f4918862
C0ldstudy <xjc04@qq.com>
2025-03-17 09:51:07 -0700

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240909.174723';
+const cacheName = 'chirpy-20250318.075058';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
